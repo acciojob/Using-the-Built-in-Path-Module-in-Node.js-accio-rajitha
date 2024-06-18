0e4d9280cd4e507a30ff22b760bb15b0b162fe94
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-06-18 14:35:07 +0530

    code added

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ const path = require('path');
 // TODO: Use path.join to join two file path segments
 
 // TODO: Print the resulting path to the console
+if (process.argv.length !== 4) {
+    console.error('Usage: node joinPaths.js <path1> <path2>');
+    process.exit(1); 
+  }
+  
+  
+  const path1 = process.argv[2];
+  const path2 = process.argv[3];
+  
+  
+  const joinedPath = path.join(path1, path2);
+  
+  
+  console.log('Joined path:', joinedPath);
+  
+  
+  module.exports = joinedPath;
